602a513526fa2801c32afb02e0503d7e5731dd81
GreatVV <marinegreatvv@yahoo.com>
2013-12-08 04:59:22 +0700

    Try add cropping

---
+++ Assets/Scripts/Level/Utility/CropSprite.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class CropSprite : MonoBehaviour
+{                                       
+    public SpriteRenderer SpriteRenderer;
+    public Rect OriginalRect;
+    // Use this for initialization
+	void Start ()
+	{
+	    OriginalRect = SpriteRenderer.sprite.rect;
+	    //SpriteRenderer.sprite.rect = OriginalRect;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
