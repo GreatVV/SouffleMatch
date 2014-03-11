32180c8d4c5b1ceb0f6f73d10b1e1ad5d7a168d0
GreatVV <v.rotterdamskiy@zmeke.com>
2014-03-11 20:34:48 +0400

    Start adding tutorials

---
+++ Assets/Scripts/Tutorial/Tutorial.cs
@@ -0,0 +1,38 @@
+﻿using TutorialSpace;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    public void Begin()
+    {
+        currentPage = startPage;
+        currentPage.Show();
+    }
+
+    public void End()
+    {
+        currentPage.Hide();
+        currentPage.End -= OnPageEnd;
+    }
+
+    [SerializeField]
+    private TutorialPage startPage;
+
+    private TutorialPage currentPage;
+    public static int[] blockedRows;
+    public static int[] blockedColumns;
+
+    void OnPageEnd(TutorialPage page)
+    {
+        page.End -= OnPageEnd;
+        page.Hide();
+        if (page.NextPage)
+        {
+            currentPage = page.NextPage;
+            currentPage.End += OnPageEnd;
+            currentPage.Show();
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/Tutorial/TutorialPage.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+
+namespace TutorialSpace 
+{
+    internal class TutorialPage : MonoBehaviour
+    {
+        [Serializable]
+        public class Position
+        {
+            public int x;
+            public int y;
+        }
+
+        public event Action<TutorialPage> End;
+
+        protected virtual void FireEnd(TutorialPage page)
+        {
+            var handler = End;
+            if (handler != null) handler(page);
+        }
+
+        #region In Inspector
+
+        public TutorialPage NextPage;
+
+        public int[] blockedRows;
+        public int[] blockedColumns;
+
+        public Position fromFingerPosition;
+        public Position toFingerPosition;
+
+        public iTweenEvent finger;
+
+        #endregion
+
+        void Awake()
+        {
+            
+        }
+
+        public void Show()
+        {
+            Tutorial.blockedRows = blockedRows;
+            Tutorial.blockedColumns = blockedColumns;
+
+            finger.transform.position = GamefieldUtility.ConvertXYToPosition((int) fromFingerPosition.x, (int) fromFingerPosition.y, Chuzzle.Scale);
+            finger.Values["position"] = GamefieldUtility.ConvertXYToPosition((int)toFingerPosition.x, (int)toFingerPosition.y, Chuzzle.Scale);
+        }
+
+        public void Hide()
+        {
+
+        }
+
+
+    }
+
+}
